19522ea5d8a5a4286bc3fc0feca550c58cbde7ad
Loann-ca <loann.cosano@gmail.com>
2022-11-10 14:08:53 +0100

    player test done

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -13,6 +13,9 @@ class PlayerTest {
         Player p = new Player();
         boolean res = p.nameIntroducedCorrect("pa u", new View());
         assertEquals(res,false);
-
+        res = p.nameIntroducedCorrect("", new View());
+        assertEquals(res,false);
+        res = p.nameIntroducedCorrect("pau", new View());
+        assertEquals(res,true);
     }
 }
