a710ccaa7b69caaae711375e6d3034ad940ec27d
Loann-ca <loann.cosano@gmail.com>
2022-11-10 12:07:20 +0100

    player do methods

--- src/Controler/Player.java
+++ src/Controler/Player.java
@@ -1,23 +1,55 @@
 package Controler;
 
+import Model.ReadWriteBD;
 import View.View;
 
-import java.io.IOException;
+import java.io.*;
 
-public class Player {
+public class Player
+{
     private String name;
     public int points;
     public Player(){}
     public void register(View view)
     {
+        view.introductionRegisterPlayer();
+        String introduced = "";
+        boolean correct = false;
+        while(!correct)
+        {
+            introduced= view.readNamePlayer();
+            correct = nameIntroducedCorrect(introduced, view);
+        }
+        this.name = introduced;
     }
     public boolean nameIntroducedCorrect(String introduced, View view)
     {
-        return false;
+        if(introduced.equals("") || !introduced.equals(introduced.replaceAll(" ", "")))//word empty or with spaces
+        {
+            view.emptyName();
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
-    public void setPoints(int points) {}
-    public void setName(String name){}
-    public int getPoints() {return 0;}
-    public String getName(){return "";}
-    public void save() throws IOException {}
+    public void setPoints(int points)
+    {
+        this.points = points;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getPoints()
+    {
+        return this.points;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void save() throws IOException {
+        ReadWriteBD.savePlayer(this, "src/Model/Ranking");
+    }
 }
